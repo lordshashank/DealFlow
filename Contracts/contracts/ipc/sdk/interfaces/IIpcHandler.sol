5bcc8dec83aba4bb0f2129b876f5473424775a3c
lordforever <shashanktrivedi1917@gmail.com>
2024-05-31 16:54:30 +0530

    ipc init

---
+++ Contracts/contracts/ipc/sdk/interfaces/IIpcHandler.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.23;
+
+import {IpcEnvelope, ResultMsg, CallMsg, IpcMsgKind} from "../../structs/CrossNet.sol";
+
+// Interface that needs to be implemented by IPC-aware contracts.
+interface IIpcHandler {
+    error CallerIsNotGateway();
+    error UnsupportedMsgKind();
+    error UnrecognizedResult();
+
+    /// @notice Entrypoint for handling xnet messages in IPC-aware contracts.
+    function handleIpcMessage(
+        IpcEnvelope calldata envelope
+    ) external payable returns (bytes memory ret);
+}
